8864b8d59d79b1dfa5a1b3514debf0e439f7015d
Synchro <marcus@synchromedia.co.uk>
2014-03-31 16:19:18 +0200

    Fix updatetemplate example

--- SmartmessagesAPIexamples.php
+++ SmartmessagesAPIexamples.php
@@ -48,34 +48,34 @@ if (file_exists('testsettings.php')) {
 }
 
 try {
-    $sm = new SmartmessagesAPI;
+    $smartmessages = new SmartmessagesAPI;
     //Login
-    $sm->login($user, $pass, $apikey, $baseurl);
+    $smartmessages->login($user, $pass, $apikey, $baseurl);
 
     //Create a campaign folder
-    $cid = $sm->addcampaign('api test campaign');
+    $cid = $smartmessages->addcampaign('api test campaign');
 
     //$sm = new SmartmessagesAPI(false); //Create an API instance
-    $sm = new SmartmessagesAPI(true); //Create an API instance in debug mode (will produce debug output)
-    $sm = new SmartmessagesAPI;
-    //Login
-    $sm->login($user, $pass, $apikey, $baseurl);
+    $smartmessages = new SmartmessagesAPI(true); //Create an API instance in debug mode (will produce debug output)
+    //$smartmessages = new SmartmessagesAPI;
+    //Log in
+    $smartmessages->login($user, $pass, $apikey, $baseurl);
 
     //Manage campaign folders
-    $cid = $sm->addcampaign('api test campaign');
+    $cid = $smartmessages->addcampaign('api test campaign');
     if ($cid > 0) {
-        $sm->updatecampaign($cid, 'updated api campaign');
-        $sm->deletecampaign($cid);
+        $smartmessages->updatecampaign($cid, 'updated api campaign');
+        $smartmessages->deletecampaign($cid);
     }
-    $campaigns = $sm->getcampaigns();
-    $mailshots = $sm->getcampaignmailshots(key($campaigns));
+    $campaigns = $smartmessages->getcampaigns();
+    $mailshots = $smartmessages->getcampaignmailshots(key($campaigns));
 
     //Manage mailing lists
-    $mlid = $sm->addlist('new test', 'description', true);
-    $lists = $sm->getlists(true); //Get all lists, not just visible ones
+    $mlid = $smartmessages->addlist('new test', 'description', true);
+    $lists = $smartmessages->getlists(true); //Get all lists, not just visible ones
     if ($mlid > 0) {
-        $r = $sm->updatelist($mlid, 'updated new test', 'updated description', false);
-        $sm->deletelist($mlid);
+        $r = $smartmessages->updatelist($mlid, 'updated new test', 'updated description', false);
+        $smartmessages->deletelist($mlid);
     }
     echo "<p>Mailing lists:</p>\n<ul>\n";
     foreach ($lists as $list) {
@@ -84,10 +84,10 @@ try {
     echo "</ul>\n";
 
     //Manage templates
-    $templates = $sm->gettemplates(true);
-    $sm->gettemplate(key($templates));
+    $templates = $smartmessages->gettemplates(true);
+    $smartmessages->gettemplate(key($templates));
     //Create a new template
-    $t1 = $sm->addtemplate(
+    $template1_id = $smartmessages->addtemplate(
         'apitest',
         '<html><head></head><body><h1>HTML</h1></body></html>',
         'plain',
@@ -97,10 +97,11 @@ try {
         'en',
         false
     );
-    $t2 = $sm->addtemplatefromurl('apitest url', 'http://www.google.com/', 'mysubject', 'Grabbed from google');
+    //Create a new template by loading it from a URL
+    $template2_id = $smartmessages->addtemplatefromurl('apitest url', 'http://www.google.com/', 'mysubject', 'Grabbed from google');
     //Update the template we created earlier by adding an image and asking it to import the image and rewrite its URL
-    $t1 = $sm->updatetemplate(
-        $t1,
+    $template1_id = $smartmessages->updatetemplate(
+        $template1_id,
         'apitest',
         '<html><head></head><body><h1>HTML <img alt="Butterfly" src="http://www.smartmessages.net/images/butterfly.png"></h1></body></html>',
         'plain',
@@ -110,33 +111,33 @@ try {
         'en',
         true
     );
-    $sm->gettemplate($t1);
-    $sm->gettemplate($t2);
+    $smartmessages->gettemplate($template1_id);
+    $smartmessages->gettemplate($template2_id);
     //Clean up
-    $sm->deletetemplate($t2);
-    $sm->deletetemplate($t1);
+    $smartmessages->deletetemplate($template2_id);
+    $smartmessages->deletetemplate($template1_id);
 
     //Get / Set Callback URL
-    $url = $sm->getcallbackurl();
+    $url = $smartmessages->getcallbackurl();
     echo "<p>Callback URL is $url</p>\n";
     try {
-        $sm->setcallbackurl('http://www.example.com/callback.php'); //Valid URL test
-        $sm->setcallbackurl('blah'); //Invalid URL test
+        $smartmessages->setcallbackurl('http://www.example.com/callback.php'); //Valid URL test
+        $smartmessages->setcallbackurl('blah'); //Invalid URL test
     } catch (Exception $e) {
         echo "</p>Callback URL set failed (as expected)</p>";
     }
-    $url = $sm->getcallbackurl();
+    $url = $smartmessages->getcallbackurl();
     echo "<p>Callback URL is now $url</p>\n";
 
     //Email address validation
     $address = 'rubbish';
-    if ($sm->validateaddress($address)) {
+    if ($smartmessages->validateaddress($address)) {
         echo "'$address' is valid<br />";
     } else {
         echo "'$address' is not valid<br />";
     }
     $address = 'valid@example.com';
-    if ($sm->validateaddress($address)) {
+    if ($smartmessages->validateaddress($address)) {
         echo "'$address' is valid<br />";
     } else {
         echo "'$address' is not valid<br />";
@@ -144,9 +145,9 @@ try {
 
     //Subscribe
     try {
-        $sm->subscribe($user, $testlistid, 'The Dude');
+        $smartmessages->subscribe($user, $testlistid, 'The Dude');
         echo "<p>Subscribed OK</p>";
-        $sm->subscribe($user, $testlistid, 'The Dude'); //This will fail as it's already subscribed
+        $smartmessages->subscribe($user, $testlistid, 'The Dude'); //This will fail as it's already subscribed
         echo "<p>Subscribed OK</p>";
     } catch (Exception $e) {
         echo "</p>Subscribe failed (as expected)</p>";
@@ -154,16 +155,16 @@ try {
 
     //Unsubscribe
     try {
-        $sm->unsubscribe($user, $testlistid);
+        $smartmessages->unsubscribe($user, $testlistid);
         echo "<p>Unsubscribed OK</p>";
-        $sm->unsubscribe($user, $testlistid); //This will fail as it's already unsubscribed
+        $smartmessages->unsubscribe($user, $testlistid); //This will fail as it's already unsubscribed
         echo "<p>Unsubscribed OK</p>";
     } catch (Exception $e) {
         echo "</p>Unsubscribe failed (as expected)</p>";
     }
 
     //Get unsubscribes
-    $unsubs = $sm->getlistunsubs($testlistid);
+    $unsubs = $smartmessages->getlistunsubs($testlistid);
     echo "<p>Unsubscribes:</p>\n<ul>\n";
     foreach ($unsubs as $unsub) {
         echo "<li>{$unsub['address']}</li>\n";
@@ -171,15 +172,15 @@ try {
     echo "</ul>\n";
 
     //Get spam reporters
-    $spamreporters = $sm->getspamreporters();
+    $spamreporters = $smartmessages->getspamreporters();
     echo "<p>Spam reporters:</p>\n<ul>\n";
     foreach ($spamreporters as $spamreporter) {
         echo "<li>$spamreporter</li>\n";
     }
     echo "</ul>\n";
     //Get/Set user info
-    $sm->getuserinfo($user);
-    $sm->setuserinfo(
+    $smartmessages->getuserinfo($user);
+    $smartmessages->setuserinfo(
         $user,
         array(
             'firstname' => 'Joe',
@@ -190,21 +191,21 @@ try {
             'custom3' => '1 2 3'
         )
     );
-    $sm->getuserinfo($user);
+    $smartmessages->getuserinfo($user);
 
     //Get/Set import order
-    $fo = $sm->getfieldorder();
-    $sm->setfieldorder(array('emailaddress', 'firstname', 'lastname', 'custom1'));
-    $nfo = $sm->getfieldorder();
+    $fo = $smartmessages->getfieldorder();
+    $smartmessages->setfieldorder(array('emailaddress', 'firstname', 'lastname', 'custom1'));
+    $nfo = $smartmessages->getfieldorder();
     echo "<p>Field order:</p>\n<ul>\n";
     foreach ($nfo as $f) {
         echo "<li>$f</li>\n";
     }
     echo "</ul>\n";
-    $sm->setfieldorder($fo); //Reset to previous order
+    $smartmessages->setfieldorder($fo); //Reset to previous order
 
     //Get info about pending list uploads
-    $uploads = $sm->getuploads($testlistid);
+    $uploads = $smartmessages->getuploads($testlistid);
     echo "<p>Uploads:</p>\n<ul>\n";
     foreach ($uploads as $upload) {
         echo "<li>" . var_dump($upload) . "</li>\n";
@@ -212,23 +213,23 @@ try {
     echo "</ul>\n";
 
     //Upload a mailing list
-    $uploadid = $sm->uploadlist($testlistid, 'testlist.csv', 'API upload', true, true, true);
+    $uploadid = $smartmessages->uploadlist($testlistid, 'testlist.csv', 'API upload', true, true, true);
     if ((integer)$uploadid > 0) {
         echo "Uploaded list OK<br />";
         //Get upload info
         echo "<p>Upload info:</p>\n";
-        $uploadinfo = $sm->getuploadinfo($testlistid, (integer)$uploadid);
+        $uploadinfo = $smartmessages->getuploadinfo($testlistid, (integer)$uploadid);
         var_dump($uploadinfo);
     } else {
         echo "List upload failed<br />";
     }
 
     //Get the complete contents of a mailing list as a CSV
-    $list = $sm->getlist($testlistid, true);
+    $list = $smartmessages->getlist($testlistid, true);
     var_dump($list);
 
     //Get unsubscribes
-    $unsubs = $sm->getlistunsubs($testlistid);
+    $unsubs = $smartmessages->getlistunsubs($testlistid);
     echo "<p>Unsubscribes:</p>\n<ul>\n";
     foreach ($unsubs as $unsub) {
         echo "<li>{$unsub['address']}</li>\n";
@@ -249,18 +250,18 @@ try {
 
     //Get data relating to a mailshot as CSVs
     if ($mailshotid > 0) {
-        $bounces = $sm->getmailshotbounces($mailshotid, true);
+        $bounces = $smartmessages->getmailshotbounces($mailshotid, true);
         var_dump($bounces);
-        $opens = $sm->getmailshotopens($mailshotid, true);
+        $opens = $smartmessages->getmailshotopens($mailshotid, true);
         var_dump($opens);
-        $clicks = $sm->getmailshotclicks($mailshotid, true);
+        $clicks = $smartmessages->getmailshotclicks($mailshotid, true);
         var_dump($clicks);
-        $unsubs = $sm->getmailshotunsubs($mailshotid, true);
+        $unsubs = $smartmessages->getmailshotunsubs($mailshotid, true);
         var_dump($unsubs);
     }
 
     //Log out
-    $sm->logout();
+    $smartmessages->logout();
 } catch (SmartmessagesAPIException $e) {
     echo '<h1>Exception caught</h1><p>An error (' . $e->getCode() . ') occurred: ' . $e->getMessage() . "</p>\n";
 }
